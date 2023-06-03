944e2ca07844ae791db566e5c2b142d5b03875f1
Golibjon Turdaliev <gturdaliev@corp.csolutions.lv>
2023-06-04 00:17:44 +0300

    added cut project

---
+++ Bulky/Bulku.Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bulky.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [DisplayName("Category Name")]
+        public string? Name { get; set; }
+        [DisplayName("Display Name")]
+        [Range(1,1000)]
+        public int DisplayOrder { get; set; }
+    }
+}
