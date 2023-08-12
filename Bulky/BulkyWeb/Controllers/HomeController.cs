e3bf2fab97451c44334a29ad877366c53d6426c3
Golibjon Turdaliev <gturdaliev@corp.csolutions.lv>
2023-08-12 15:05:27 +0500

    added new cruud

--- Bulky/BulkyWeb/Controllers/HomeController.cs
+++ Bulky/BulkyWeb/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Bulky.Models;
+﻿using Bulky.DataAccess.Repository.IRepsitory;
+using Bulky.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,16 +7,27 @@ namespace BulkyWeb.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IUnitOfWork unitOfWork )
         {
+            _unitOfWork = unitOfWork;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category"); 
+
+            return View(productList);
+        }
+
+        public IActionResult Details(int productId)
+        {
+            Product product = _unitOfWork.Product.Get(u => u.Id == productId);
+
+            return View(product);
         }
 
         public IActionResult Privacy()
